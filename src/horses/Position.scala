47cde3ba4008a462cc80861f79ca2d81bf5e188d
Javier Iserte <jiserte@unq.edu.ar>
2012-03-24 15:02:52 -0300

    Modification in Horses

--- src/horses/Position.scala
+++ src/horses/Position.scala
@@ -10,7 +10,11 @@ abstract class HorseColor
 case class White() extends HorseColor
 case class Black() extends HorseColor
 
-
+/**
+ * 
+ * ver video: http://www.youtube.com/watch?v=Yqhfneh3NLc
+ *
+ */
 class Horse (var pos: Position, var color:HorseColor ) {
 	override def toString = {
 		pos+print(color)
@@ -27,6 +31,12 @@ class Horse (var pos: Position, var color:HorseColor ) {
 	def move(newPos: Position): Unit = {
 		this.pos = newPos
 	}
+	
+	def equals (other: Horse):Boolean = {
+		(this.position.x equals other.position.x) &&
+		(this.position.y equals other.position.y) &&
+		this.color      == other.color
+	}
 }
 
 
@@ -42,7 +52,7 @@ class horsesGame(tablero: Array[Position], inicio: Array[Horse], val paraGanar: 
 	}
 	
 	def pr(t:(Boolean, Boolean)): Unit = {
-		println("Movimiento v�lido: "+ t._1 + " Gana el juego: " + t._2)
+		println("Movimiento válido: "+ t._1 + " Gana el juego: " + t._2)
 	}
 	/**
 	 * 
@@ -54,18 +64,18 @@ class horsesGame(tablero: Array[Position], inicio: Array[Horse], val paraGanar: 
 		val dx:Int = (horse.position.x - newPosition.x).abs
 		val dy:Int = (horse.position.y - newPosition.y).abs
 		
-		println("dx =" + dx + " dy="+dy)
+//		println("dx =" + dx + " dy="+dy)
 		
 		// Verifica que el movimiento que se quiere hacer es correcto
 		if (dx==2&&dy==1||dx==1&&dy==2) {
-			println("primer punto de control pasado")
+//			println("primer punto de control pasado")
 			
 			// verifica que el Horse pasado por par�metro est� realmente en el tablero
 			var isInGame = false
 			inicio.foreach((h:Horse) => isInGame = (isInGame || h==horse))
-			inicio.foreach((h:Horse) => println(h))
-			println("tamaño: " +inicio.length)
-			println("esta en juego:" + isInGame)
+//			inicio.foreach((h:Horse) => println(h))
+//			println("tamaño: " +inicio.length)
+//			println("esta en juego:" + isInGame)
 			
 			if (isInGame) {
 				// verifica que la posici�n a la que se quiere ir no est� ocupada
@@ -84,6 +94,7 @@ class horsesGame(tablero: Array[Position], inicio: Array[Horse], val paraGanar: 
 					inicio.foreach((hi:Horse) => 
 						paraGanar.foreach((hf:Horse) => matches = matches + {if(hi equals hf) 1 else 0})
 					)
+
 					if (matches==4) (true,true)
 					else (true, false)
 					
@@ -104,13 +115,13 @@ object horsesApp extends Application {
 	override def main(args : Array[String]): Unit = {
 		
 		val pos1 = new Position(2,3);
-		println("pos1:" + pos1.toString);
+//		println("pos1:" + pos1.toString);
 		
 		val c1 = new Horse(pos1,Black())
 		val c2 = new Horse(new Position(4,5),White())
 		
-		println(c1)
-		println(c2)
+//		println(c1)
+//		println(c2)
 		
 		val tablero = Array {
 			new Position (1,1); 
@@ -131,6 +142,7 @@ object horsesApp extends Application {
 		val WH1 = new Horse(new Position(2,2),White())
 		val WH2 = new Horse(new Position(3,4),White())
 		
+		val WH3 = new Horse(new Position(3,4),White())
 		
 		val inicio = Array (
 			BH1,
@@ -146,16 +158,58 @@ object horsesApp extends Application {
 			new Horse(new Position(3,4),Black())
 		)
 	
+		print(WH2.equals(WH3))
+		
 		val game = new horsesGame(tablero, inicio, fin)
 		
 
 		val moves = List[(Horse,Position)](
 			(BH1, new Position(2,3)),
-			(BH1, new Position(3,1))
+			(BH1, new Position(3,1)),
+			(BH1, new Position(1,2)),
+			(BH1, new Position(3,3)),
+			(BH1, new Position(2,1)),
+			(WH1, new Position(4,1)),
+			(WH1, new Position(3,3)),
+			(WH1, new Position(1,2)),
+			(WH1, new Position(3,1)),
+			(WH2, new Position(2,2)),
+			(WH2, new Position(4,1)),
+			(WH2, new Position(3,3)),
+			(WH2, new Position(1,2)),
+			(BH1, new Position(3,3)),
+			(BH1, new Position(4,1)),
+			(BH1, new Position(2,2)),
+			(BH1, new Position(3,4)),
+			(WH2, new Position(3,3)),
+			(WH2, new Position(4,1)),
+			(WH2, new Position(2,2)),
+			(WH1, new Position(1,2)),
+			(WH1, new Position(3,3)),
+			(WH1, new Position(4,1)),
+			(BH2, new Position(1,1)),
+			(BH2, new Position(2,3)),
+			(BH2, new Position(3,1)),
+			(BH2, new Position(1,2)),
+			(BH2, new Position(3,3)),
+			(BH2, new Position(2,1)),
+			(WH1, new Position(3,3)),			
+			(WH1, new Position(1,2)),	
+			(WH1, new Position(3,1)),
+			(WH1, new Position(2,3)),
+			(WH1, new Position(1,1)),
+			(WH1, new Position(3,2)),
+			(WH2, new Position(4,1)),			
+			(WH2, new Position(3,3)),			
+			(WH2, new Position(1,2)),	
+			(WH2, new Position(3,1)),
+			(WH2, new Position(2,3)),
+			(WH2, new Position(1,1)),
+			(BH2, new Position(3,3)),
+			(BH2, new Position(4,1)),
+			(BH2, new Position(2,2))			
 		)
 		
 		moves.foreach( (t:(Horse,Position)) => game.pr(game.move(t._1,t._2)))		
-		
-		println(BH1.position)
 	}
 }
